fb998aaa6d39e23d76787afc671ee50ccc532e70
David Allemang <david.allemang@kitware.com>
2023-12-21 13:30:46 -0500

    Add USE_SYSTEM_fmt option
    Note about download binaries

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -1,13 +1,28 @@
 include(ExternalProject)
 
-ExternalProject_Add(
-    fmt
-    GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-    GIT_TAG 10.1.0
-    GIT_PROGRESS ON
-    CMAKE_CACHE_ARGS
-    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-)
+if (NOT USE_SYSTEM_fmt)
+  ExternalProject_Add(
+      fmt
+      GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+      GIT_TAG 10.1.0
+      GIT_PROGRESS ON
+      INSTALL_COMMAND ""
+  )
+
+  ExternalProject_Get_Property(fmt BINARY_DIR)
+  set(fmt_DIR ${BINARY_DIR})
+else ()
+  ExternalProject_Add(
+      fmt
+      DOWNLOAD_COMMAND ""
+      CONFIGURE_COMMAND ""
+      BUILD_COMMAND ""
+      INSTALL_COMMAND ""
+  )
+
+  unset(fmt_DIR CACHE)
+  find_package(fmt REQUIRED)
+endif ()
 
 ExternalProject_Add(
     inner
@@ -16,6 +31,6 @@ ExternalProject_Add(
     INSTALL_COMMAND ""
     CMAKE_CACHE_ARGS
     -DUSE_SUPERBUILD:BOOL=OFF
-    -DCMAKE_PREFIX_PATH:PATH=${CMAKE_CURRENT_BINARY_DIR}
+    -Dfmt_DIR:PATH=${fmt_DIR}
     DEPENDS fmt
 )
